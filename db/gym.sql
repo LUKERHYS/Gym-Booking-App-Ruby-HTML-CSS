9e43b411310305616b03bcfc7d88f67bdb51e67d
lukerhys <hello@lukemelvin.com>
2020-02-08 17:35:57 +0000

    Creation of MVP sql file

---
+++ db/gym.sql
@@ -0,0 +1,21 @@
+DROP TABLE bookings;
+DROP TABLE members;
+DROP TABLE sessions;
+
+CREATE TABLE sessions (
+  id SERIAL PRIMARY KEY,
+  type VARCHAR(255),
+  trainer VARCHAR(255),
+  room INT
+  );
+CREATE TABLE members (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  goal VARCHAR(255)
+  );
+CREATE TABLE bookings (
+  id SERIAL PRIMARY KEY,
+  session_id INT REFERENCES sessions(id),
+  member_id INT REFERENCES members(id),
+  );
